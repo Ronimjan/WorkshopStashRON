b7abdc0f41d31a07d3d8c48033086e5fabe15ab5
Ronimjan <ronimjan.programmer@gmail.com>
2021-04-06 00:26:23 +0200

    Fixed UI Input/Output Control issue,

--- WorkshopStashRON/CampaignChanger.cs
+++ WorkshopStashRON/CampaignChanger.cs
@@ -22,12 +22,15 @@ namespace WorkshopStashRON
 
         private void OnAfterNewGameCreated(CampaignGameStarter starter)
         {
+            var stash = GetCurrentSettlementStash();
+            MBTextManager.SetTextVariable("STASH_INPUT", stash.InputTrue ? "Yes" : "No");
+            MBTextManager.SetTextVariable("STASH_OUTPUT", stash.OutputTrue ? "Yes" : "No");
             starter.AddGameMenu("workshop_manage", "You are visiting your workshops.", new OnInitDelegate(args => { args.MenuTitle = new TextObject("Workshops", null); }), GameOverlays.MenuOverlayType.SettlementWithBoth, GameMenu.MenuFlags.none, (object)null);
             starter.AddGameMenuOption("town", "Workshop_Stash", "Manage your workshops", new GameMenuOption.OnConditionDelegate(HasAnyWorkshops), new GameMenuOption.OnConsequenceDelegate(x => GameMenu.SwitchToMenu("workshop_manage")), false, 6, false);
             starter.AddGameMenuOption("workshop_manage", "Workshop_Stash_Browse", "Browse your Stash", new GameMenuOption.OnConditionDelegate(StashCondition), new GameMenuOption.OnConsequenceDelegate(StashConsequence), false, -1, false);
-            starter.AddGameMenuOption("workshop_manage", "Workshop_Toggle_Input", "Use materials from stash: {INPUT_STASH}" ,new GameMenuOption.OnConditionDelegate(ProductionCondition), new GameMenuOption.OnConsequenceDelegate(ToggleInput), false, -1, false);
-            starter.AddGameMenuOption("workshop_manage", "Workshop_Toggle_Output", "Place produced goods in stash: {OUTPUT_STASH}", new GameMenuOption.OnConditionDelegate(ProductionCondition), new GameMenuOption.OnConsequenceDelegate(ToggleOutput), false, -1, false);
-            starter.AddGameMenuOption("workshop_manage", "Workshop_Leave", "Back to towncenter", new GameMenuOption.OnConditionDelegate(BackCondition), new GameMenuOption.OnConsequenceDelegate(_ => GameMenu.SwitchToMenu("town")), true, -1, false);
+            starter.AddGameMenuOption("workshop_manage", "Workshop_Toggle_Input", "Use materials from stash: {STASH_INPUT}" ,new GameMenuOption.OnConditionDelegate(ProductionCondition), new GameMenuOption.OnConsequenceDelegate(ToggleInput), false, -1, false);
+            starter.AddGameMenuOption("workshop_manage", "Workshop_Toggle_Output", "Put produced goods into stash: {STASH_OUTPUT}", new GameMenuOption.OnConditionDelegate(ProductionCondition), new GameMenuOption.OnConsequenceDelegate(ToggleOutput), false, -1, false);
+            starter.AddGameMenuOption("workshop_manage", "Workshop_Leave", "Back to town center", new GameMenuOption.OnConditionDelegate(BackCondition), new GameMenuOption.OnConsequenceDelegate(_ => GameMenu.SwitchToMenu("town")), true, -1, false);
             
         }
 
